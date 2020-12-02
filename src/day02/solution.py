2709bc9e797ee229609084c3d324041d09a37951
noaione <noaione0809@gmail.com>
2020-12-02 14:35:08 +0700

    day02: make into function

--- src/day02/solution.py
+++ src/day02/solution.py
@@ -1,5 +1,4 @@
-in_data = [num.rstrip() for num in open("input", "r").readlines() if num]
-# in_data = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
+import typing as t
 
 
 def remap(cpass: str):
@@ -14,24 +13,33 @@ def remap(cpass: str):
     }
 
 
-# Part A
-in_data = [remap(c) for c in in_data]
-print(len(in_data))
-valid = 0
-for data in in_data:
-    total_char = data["pass"].count(data["code"])
-    if total_char >= data["s"] and total_char <= data["e"]:
-        valid += 1
-print(f"Part A: {valid}")
+def solve_a(input_mapped: t.List[dict]) -> int:
+    valid = 0
+    for data in input_mapped:
+        total_char = data["pass"].count(data["code"])
+        if total_char >= data["s"] and total_char <= data["e"]:
+            valid += 1
+    return valid
 
-valid_b = 0
-for data in in_data:
-    fn_val = data["pass"][data["s"] - 1] == data["code"]
-    ln_val = data["pass"][data["e"] - 1] == data["code"]
-    if fn_val:
-        if not ln_val:
-            valid_b += 1
-    elif ln_val:
-        if not fn_val:
-            valid_b += 1
-print(f"Part B: {valid_b}")
+
+def solve_b(input_mapped: t.List[dict]) -> int:
+    valid = 0
+    for data in input_mapped:
+        fn_val = data["pass"][data["s"] - 1] == data["code"]
+        ln_val = data["pass"][data["e"] - 1] == data["code"]
+        if fn_val:
+            if not ln_val:
+                valid += 1
+        elif ln_val:
+            if not fn_val:
+                valid += 1
+    return valid
+
+
+if __name__ == "__main__":
+    in_data = [num.rstrip() for num in open("input", "r").readlines() if num]
+    # in_data = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]
+    mapped_data = [remap(c) for c in in_data]
+    print(mapped_data)
+    print(f"Part A: {solve_a(mapped_data)}")
+    print(f"Part B: {solve_b(mapped_data)}")
